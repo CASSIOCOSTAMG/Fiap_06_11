c967ff7e30bb11ae6945ab8321ed20bef0b285b8
cassiocostamg <cassiomilkmg@gmail.com>
2024-10-03 12:50:46 -0300

    Update ConnectionFactory.java

--- Fintech_Cap11/src/main/java/br/com/fiap/fintech/factory/ConnectionFactory.java
+++ Fintech_Cap11/src/main/java/br/com/fiap/fintech/factory/ConnectionFactory.java
@@ -7,8 +7,8 @@ import java.sql.SQLException;
 public class ConnectionFactory {
 
     private static final String URL =  "jdbc:oracle:thin:@oracle.fiap.com.br:1521:orcl";
-    private static final String USUARIO = "rm557226" ;  // "pf0392";
-    private static final String SENHA =   "080378";  //"fiap";
+    private static final String USUARIO = "rm" ;  // "pf0392";
+    private static final String SENHA =   "";  //"fiap";
 
     // metodo para obter uma conexao com o banco de dados
     public static Connection getConnection() throws SQLException {
